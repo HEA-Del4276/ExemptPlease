f2066e3c0106183d580a3df258db884f9e102f09
HEA-Del4276 <202410364@fit.edu.ph>
2024-11-22 13:43:31 +0800

    dog's name

--- ExemptSample.cpp
+++ ExemptSample.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main() {
 
-	string favorite;
+	string favorite, dog;
 
 	cout << "What is your favorite anime?: ";
 	cin >> favorite;
+
+	cout << "What is your dog's name?: ";
+	cin >> dog;
 
 	return 0;
 }
